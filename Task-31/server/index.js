09463f64931fca039a4d25a038a5f64df879ccd5
Sidd444 <siddharthabharaliassam@gmail.com>
2024-09-06 15:12:22 +0530

    j

--- Task-31/server/index.js
+++ Task-31/server/index.js
@@ -12,4 +12,4 @@ app.use('/api/auth', authRoutes);
 app.use('/api/protected', authMiddleware, protectedRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT} for task-32`));
